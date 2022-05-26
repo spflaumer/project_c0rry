cbaecd48005f699dbf72910b45a9fd0341a030ef
1nekomata <73189703+1nekomata@users.noreply.github.com>
2022-05-26 22:42:12 +0200

    Add License

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,19 @@
+;    boot.asm - Initial 512 bytes of the Legacy BIOS Bootloader for c0rry 
+;    Copyright (C) 2022  1nekomata
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 [org 0x7c00]
 
 mov bp, 0x7c00
